dfc47c505f611c810e87ed163d264eacc3ca2dd8
libsgh <woiyyng@gmail.com>
2024-09-24 10:02:28 +0800

    Update cron

--- chrome_auto_update.go
+++ chrome_auto_update.go
@@ -53,8 +53,9 @@ func chromeAutoUpdate(a fyne.App, win fyne.Window, data *SettingsData) {
 var runFlag = 0
 
 func addUpdateCron(data *SettingsData) {
-	spec := "* * */1 * * *" // 每隔5s执行一次，cron格式（秒，分，时，天，月，周）
+	spec := "0 0 0/1 * * ?"
 	_, _ = cronManager.AddFunc(spec, func() {
+		fmt.Println(time.Now())
 		chromeInUse := isProcessExist("chrome.exe")
 		if runFlag == 1 || chromeInUse {
 			return
